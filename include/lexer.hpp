530115c2782415fac5aec1238683bcbc539ba73f
Gustaf Brostedt <gustaf@saporo.io>
2024-06-10 12:30:08 +0200

    fix identifier strings

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -8,18 +8,18 @@
 
 namespace gbpl0 {
 
-constexpr char var[] = "var";
-constexpr char odd[] = "odd";
-constexpr char constex[] = "const";
-constexpr char ifcond[] = "ifcond";
-constexpr char docond[] = "docond";
-constexpr char thencond[] = "thencond";
-constexpr char elsecond[] = "elsecond";
-constexpr char call[] = "call";
-constexpr char begin[] = "begin";
-constexpr char floop[] = "for";
-constexpr char wloop[] = "while";
-constexpr char procedure[] = "procedure";
+constexpr char kw_var[] = "var";
+constexpr char kw_odd[] = "odd";
+constexpr char kw_const[] = "const";
+constexpr char kw_cond[] = "if";
+constexpr char kw_do[] = "do";
+constexpr char kw_then[] = "then";
+constexpr char kw_else[] = "else";
+constexpr char kw_call[] = "call";
+constexpr char kw_begin[] = "begin";
+constexpr char kw_for[] = "for";
+constexpr char kw_while[] = "while";
+constexpr char kw_procedure[] = "procedure";
 
 class Lexer {
 public:
@@ -31,22 +31,23 @@ public:
   ~Lexer() = default;
 
   Lexeme next();
-  void expect(TOKEN token);
   void read_file(const std::string &filename);
 
   // debugging
   void dump_lexeme(const Lexeme &lex) const;
+  void parse_error(const std::string &err) const;
+  bool at_end() const;
 
 private:
   // parse expressions
+  void parse_whitespace();
   void parse_comment();
-  void parse_ident();
-  void parse_number();
+  Lexeme parse_ident();
+  Lexeme parse_number();
 
   // helpers
   char peek() const;
   char peek_next() const;
-  void parse_error(const std::string &err) const;
   void match(const char rhs);
   char advance();
 
@@ -56,8 +57,9 @@ private:
   TOKEN _token;
 
   // position tracking
-  std::size_t _linum = 0;
+  std::size_t _linum = 1;
   std::size_t _lpos = 1;
+  std::size_t _token_line_pos = 0;
   std::size_t _token_start = 0;
   std::size_t _pos = 0;
   std::size_t _filesize = 0;
