914f1608cde370d24eca092c217b60cdde0a6cfa
Gustaf Brostedt <gustaf@saporo.io>
2024-06-20 23:50:49 +0200

    update literal

--- include/plzerow/ast_nodes.hpp
+++ include/plzerow/ast_nodes.hpp
@@ -2,10 +2,10 @@
 #pragma once
 
 #include <memory>
-#include <string>
-#include <vector>
-#include <variant>
 #include <plzerow/token_type.hpp>
+#include <string>
+#include <variant>
+#include <vector>
 
 namespace plzerow {
 
@@ -25,116 +25,129 @@ struct Primary;
 struct Literal;
 
 struct Grouping {
-    Grouping(std::unique_ptr<ASTNode> expression) : _expression(std::move(expression)) {}
-    Grouping(Grouping&&) = default;
-    Grouping& operator=(Grouping&&) = default;
-    std::unique_ptr<ASTNode> _expression;
+  Grouping(std::unique_ptr<ASTNode> expression)
+      : _expression(std::move(expression)) {}
+  Grouping(Grouping&&) = default;
+  Grouping& operator=(Grouping&&) = default;
+  std::unique_ptr<ASTNode> _expression;
 };
 
 struct Equality {
-    Equality(std::unique_ptr<ASTNode> left, TOKEN op, std::unique_ptr<ASTNode> right) : _left(std::move(left)), _op(op), _right(std::move(right)) {}
-    Equality(Equality&&) = default;
-    Equality& operator=(Equality&&) = default;
-    std::unique_ptr<ASTNode> _left;
-    TOKEN _op;
-    std::unique_ptr<ASTNode> _right;
+  Equality(std::unique_ptr<ASTNode> left, TOKEN op,
+           std::unique_ptr<ASTNode> right)
+      : _left(std::move(left)), _op(op), _right(std::move(right)) {}
+  Equality(Equality&&) = default;
+  Equality& operator=(Equality&&) = default;
+  std::unique_ptr<ASTNode> _left;
+  TOKEN _op;
+  std::unique_ptr<ASTNode> _right;
 };
 
 struct Comparison {
-    Comparison(std::unique_ptr<ASTNode> left, TOKEN op, std::unique_ptr<ASTNode> right) : _left(std::move(left)), _op(op), _right(std::move(right)) {}
-    Comparison(Comparison&&) = default;
-    Comparison& operator=(Comparison&&) = default;
-    std::unique_ptr<ASTNode> _left;
-    TOKEN _op;
-    std::unique_ptr<ASTNode> _right;
+  Comparison(std::unique_ptr<ASTNode> left, TOKEN op,
+             std::unique_ptr<ASTNode> right)
+      : _left(std::move(left)), _op(op), _right(std::move(right)) {}
+  Comparison(Comparison&&) = default;
+  Comparison& operator=(Comparison&&) = default;
+  std::unique_ptr<ASTNode> _left;
+  TOKEN _op;
+  std::unique_ptr<ASTNode> _right;
 };
 
 struct Term {
-    Term(std::unique_ptr<ASTNode> left, TOKEN op, std::unique_ptr<ASTNode> right) : _left(std::move(left)), _op(op), _right(std::move(right)) {}
-    Term(Term&&) = default;
-    Term& operator=(Term&&) = default;
-    std::unique_ptr<ASTNode> _left;
-    TOKEN _op;
-    std::unique_ptr<ASTNode> _right;
+  Term(std::unique_ptr<ASTNode> left, TOKEN op, std::unique_ptr<ASTNode> right)
+      : _left(std::move(left)), _op(op), _right(std::move(right)) {}
+  Term(Term&&) = default;
+  Term& operator=(Term&&) = default;
+  std::unique_ptr<ASTNode> _left;
+  TOKEN _op;
+  std::unique_ptr<ASTNode> _right;
 };
 
 struct Unary {
-    Unary(TOKEN op, std::unique_ptr<ASTNode> right) : _op(op), _right(std::move(right)) {}
-    Unary(Unary&&) = default;
-    Unary& operator=(Unary&&) = default;
-    TOKEN _op;
-    std::unique_ptr<ASTNode> _right;
+  Unary(TOKEN op, std::unique_ptr<ASTNode> right)
+      : _op(op), _right(std::move(right)) {}
+  Unary(Unary&&) = default;
+  Unary& operator=(Unary&&) = default;
+  TOKEN _op;
+  std::unique_ptr<ASTNode> _right;
 };
 
 struct Binary {
-    Binary(std::unique_ptr<ASTNode> left, TOKEN op, std::unique_ptr<ASTNode> right) : _left(std::move(left)), _op(op), _right(std::move(right)) {}
-    Binary(Binary&&) = default;
-    Binary& operator=(Binary&&) = default;
-    std::unique_ptr<ASTNode> _left;
-    TOKEN _op;
-    std::unique_ptr<ASTNode> _right;
+  Binary(std::unique_ptr<ASTNode> left, TOKEN op,
+         std::unique_ptr<ASTNode> right)
+      : _left(std::move(left)), _op(op), _right(std::move(right)) {}
+  Binary(Binary&&) = default;
+  Binary& operator=(Binary&&) = default;
+  std::unique_ptr<ASTNode> _left;
+  TOKEN _op;
+  std::unique_ptr<ASTNode> _right;
 };
 
 struct Factor {
-    Factor(std::unique_ptr<ASTNode> left, TOKEN op, std::unique_ptr<ASTNode> right) : _left(std::move(left)), _op(op), _right(std::move(right)) {}
-    Factor(Factor&&) = default;
-    Factor& operator=(Factor&&) = default;
-    std::unique_ptr<ASTNode> _left;
-    TOKEN _op;
-    std::unique_ptr<ASTNode> _right;
+  Factor(std::unique_ptr<ASTNode> left, TOKEN op,
+         std::unique_ptr<ASTNode> right)
+      : _left(std::move(left)), _op(op), _right(std::move(right)) {}
+  Factor(Factor&&) = default;
+  Factor& operator=(Factor&&) = default;
+  std::unique_ptr<ASTNode> _left;
+  TOKEN _op;
+  std::unique_ptr<ASTNode> _right;
 };
 
 struct Primary {
-    Primary(std::unique_ptr<ASTNode> value) : _value(std::move(value)) {}
-    Primary(Primary&&) = default;
-    Primary& operator=(Primary&&) = default;
-    std::unique_ptr<ASTNode> _value;
+  Primary(std::unique_ptr<ASTNode> value) : _value(std::move(value)) {}
+  Primary(Primary&&) = default;
+  Primary& operator=(Primary&&) = default;
+  std::unique_ptr<ASTNode> _value;
 };
 
 struct Literal {
-    Literal(std::string value) : _value(value) {}
-    Literal(Literal&&) = default;
-    Literal& operator=(Literal&&) = default;
-    std::string _value;
+  Literal(std::string value) : _value(value) {}
+  Literal(bool value) : _value(value) {}
+  Literal(std::int32_t value) : _value(value) {}
+  Literal(double value) : _value(value) {}
+
+  Literal(Literal&&) = default;
+  Literal& operator=(Literal&&) = default;
+
+  bool as_bool() const;
+  const std::string& as_string() const;
+  std::int32_t as_int() const;
+  double as_double() const;
+
+  TOKEN _type;
+  std::variant<std::int32_t, double, std::string, bool> _value;
 };
 
-
 class ASTNode {
-public:
-    template <typename T>
-    ASTNode(std::size_t linum, std::size_t column, T &&value);
-    template <typename Visitor> auto accept(Visitor &&visitor) const;
+ public:
+  template <typename T>
+  ASTNode(std::size_t linum, std::size_t column, T&& value);
+  template <typename Visitor>
+  auto accept(Visitor&& visitor) const;
 
-    std::size_t _linum;
-    std::size_t _column;
-    std::variant<
-Grouping,
-        Equality,
-        Comparison,
-        Term,
-        Unary,
-        Binary,
-        Factor,
-        Primary,
-        Literal
-    > _value;
+  std::size_t _linum;
+  std::size_t _column;
+  std::variant<Grouping, Equality, Comparison, Term, Unary, Binary, Factor,
+               Primary, Literal>
+      _value;
 };
 
 template <typename T>
-ASTNode::ASTNode(std::size_t linum,
-                 std::size_t column, T &&value)
-     :_linum(linum), _column(column),
-      _value(std::move(value)) {}
+ASTNode::ASTNode(std::size_t linum, std::size_t column, T&& value)
+    : _linum(linum), _column(column), _value(std::move(value)) {}
 
-template <typename Visitor> auto ASTNode::accept(Visitor &&visitor) const {
-    return std::visit(std::forward<Visitor>(visitor), _value);
+template <typename Visitor>
+auto ASTNode::accept(Visitor&& visitor) const {
+  return std::visit(std::forward<Visitor>(visitor), _value);
 }
 
 template <typename T, typename... Args>
-std::unique_ptr<ASTNode> make_ast_node(std::size_t linum,
-                                       std::size_t column, Args &&...args) {
+std::unique_ptr<ASTNode> make_ast_node(std::size_t linum, std::size_t column,
+                                       Args&&... args) {
   return std::make_unique<ASTNode>(linum, column,
                                    T{std::forward<Args>(args)...});
 }
 
-} // namespace plzerow
+}  // namespace plzerow
--- src/ast_nodes.cpp
+++ src/ast_nodes.cpp
@@ -1,6 +1,18 @@
 
+#include <cassert>
 #include <plzerow/ast_nodes.hpp>
+#include <variant>
 
 namespace plzerow {
 
-} // namespace plzerow
+bool Literal::as_bool() const { return std::get<bool>(_value); }
+
+const std::string& Literal::as_string() const {
+  return std::get<std::string>(_value);
+}
+
+std::int32_t Literal::as_int() const { return std::get<std::int32_t>(_value); }
+
+double Literal::as_double() const { return std::get<double>(_value); }
+
+}  // namespace plzerow
